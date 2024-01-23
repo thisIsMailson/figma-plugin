86ed744d43e85637c7c3e9d1bfba8a1d6f8baf56
thisIsMailson <mailsonteixeiravarela@gmail.com>
2024-01-23 16:50:51 +0000

    new sample

--- assets-download/code.ts
+++ assets-download/code.ts
@@ -1,80 +1,135 @@
 // import JSZip from '../node_modules/jszip/dist/jszip.min.js';
-
-figma.showUI(__html__);
-
+// https://github.com/brianlovin/figma-export-zip/tree/main
+import JSZip from "../node_modules/jszip/dist/jszip.min.js";
+const selectedLayers = figma.currentPage.selection;
 figma.ui.onmessage = async (message) => {
-  const selectedLayers = figma.currentPage.selection;
-
-  // Check if there is at least one selected layer
-  if (selectedLayers.length > 0) {
-    // Assuming the first selected layer is the frame
-    const selectedFrame = selectedLayers[0];
-    console.log("frame t", selectedFrame.type);
-    if (selectedFrame.type === "FRAME") {
-      const frameName = selectedFrame.name;
-      console.log("Selected Frame Name:", frameName);
-
-      const exportResults = [];
-
-      for (const child of selectedFrame.children) {
-        const node = await child.exportAsync({ format: "PNG" });
-        console.log("node ->", node);
-        exportResults.push(node);
-      }
-
-      return exportResults;
-    } else {
-      console.error("Please select a frame to start the plugin.");
+  console.log("mess", selectedLayers);
+  let exportableBytes = [];
+  const nodes = selectedLayers[0].children;
+  for (let node of nodes) {
+    let { name, exportSettings } = node;
+    if (exportSettings.length === 0) {
+      exportSettings = [
+        {
+          format: "PNG",
+          suffix: "",
+          constraint: { type: "SCALE", value: 1 },
+          contentsOnly: true,
+        },
+      ];
     }
-  } else {
-    console.error(
-      "No layers are selected. Please select a frame to start the plugin."
-    );
-  }
 
+    for (let setting of exportSettings) {
+      let defaultSetting = setting;
+      const bytes = await node.exportAsync(defaultSetting);
+      exportableBytes.push({ name, setting, bytes });
+    }
+  }
+  generateZip(exportableBytes);
   figma.closePlugin();
 };
 
-// Traverse layers under the selected frame
-const traverseLayers = (layer) => {
-  for (const child of layer.children) {
-    handleLayerType(child);
-  }
-};
+function typedArrayToBuffer(array) {
+  return array.buffer.slice(
+    array.byteOffset,
+    array.byteLength + array.byteOffset
+  );
+}
 
-const downloadImage = async (imageLayer) => {
-  const imageHash = imageLayer.imageHash;
-  const image = await figma.getImageByHash(imageHash);
-
-  // Use the 'image' object to access image data
-  console.log("Downloaded Image:", image);
-};
-
-// Example: Handling different layer types
-const handleLayerType = async (layer) => {
-  switch (layer.type) {
-    case "IMAGE":
-      downloadImage(layer);
-      break;
-    case "RECTANGLE":
-    case "GROUP":
-      // Download RECTANGLE and GROUP layers as PNG
-      const exportSettings = {
-        format: "PNG",
-        constraint: { type: "SCALE", value: 2 },
-      };
-
-      try {
-        const exp = await layer.exportAsync(exportSettings);
-        console.log("your img ->", exp);
-
-        // Send the image bytes to the Figma UI
-      } catch (error) {
-        console.error("Error exporting PNG:", error);
-      }
-
-      break;
+function exportTypeToBlobType(type) {
+  switch (type) {
+    case "PDF":
+      return "application/pdf";
+    case "SVG":
+      return "image/svg+xml";
+    case "PNG":
+      return "image/png";
+    case "JPG":
+      return "image/jpeg";
     default:
-      break;
+      return "image/png";
   }
+}
+
+function exportTypeToFileExtension(type) {
+  switch (type) {
+    case "PDF":
+      return ".pdf";
+    case "SVG":
+      return ".svg";
+    case "PNG":
+      return ".png";
+    case "JPG":
+      return ".jpg";
+    default:
+      return ".png";
+  }
+}
+
+const generateZip = (exportableBytes) => { 
+  console.log("evt", exportableBytes);
+  if (!exportableBytes) return;
+
+  return new Promise((resolve) => {
+    let zip = new JSZip();
+
+    for (let data of exportableBytes) {
+      console.log("data =>", data);
+      const { bytes, name, setting } = data;
+      const cleanBytes = typedArrayToBuffer(bytes);
+      const type = exportTypeToBlobType(setting.format);
+      const extension = exportTypeToFileExtension(setting.format);
+      let blob = new Blob([cleanBytes], { type });
+      zip.file(`${name}${setting.suffix}${extension}`, blob, {
+        base64: true,
+      });
+    }
+
+    zip.generateAsync({ type: "blob" }).then((content) => {
+      const blobURL = window.URL.createObjectURL(content);
+      const link = document.createElement("a");
+      link.className = "button button--primary";
+      link.href = blobURL;
+      link.download = "export.zip";
+      link.click();
+      link.setAttribute("download", name + ".zip");
+      resolve();
+    });
+  }).then(() => {
+    window.parent.postMessage({ pluginMessage: "Done!" }, "*");
+  });
 };
+async function main(params) {
+  let exportableBytes = [];
+  const nodes = selectedLayers[0].children;
+  for (let node of nodes) {
+    let { name, exportSettings } = node;
+    if (exportSettings.length === 0) {
+      exportSettings = [
+        {
+          format: "PNG",
+          suffix: "",
+          constraint: { type: "SCALE", value: 1 },
+          contentsOnly: true,
+        },
+      ];
+    }
+
+    for (let setting of exportSettings) {
+      let defaultSetting = setting;
+      const bytes = await node.exportAsync(defaultSetting);
+      exportableBytes.push({ name, setting, bytes });
+    }
+  }
+
+  figma.showUI(__html__, { visible: false });
+  figma.ui.postMessage({ message: exportableBytes });
+
+  return new Promise((res) => {
+    figma.ui.onmessage = () => res();
+  });
+}
+
+// main("message").then((res) => console.log("closing main"));
+
+figma.showUI(__html__);
---
+++ assets-download/ui.js
@@ -0,0 +1,64 @@
+import JSZip from "../node_modules/jszip/dist/jszip.min.js";
+function typedArrayToBuffer(array) {
+    return array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset);
+}
+function exportTypeToBlobType(type) {
+    switch (type) {
+        case "PDF":
+            return "application/pdf";
+        case "SVG":
+            return "image/svg+xml";
+        case "PNG":
+            return "image/png";
+        case "JPG":
+            return "image/jpeg";
+        default:
+            return "image/png";
+    }
+}
+function exportTypeToFileExtension(type) {
+    switch (type) {
+        case "PDF":
+            return ".pdf";
+        case "SVG":
+            return ".svg";
+        case "PNG":
+            return ".png";
+        case "JPG":
+            return ".jpg";
+        default:
+            return ".png";
+    }
+}
+window.onmessage = (event) => {
+    console.log("evt", event);
+    if (!event.data.pluginMessage)
+        return;
+    const { exportableBytes } = event.data.pluginMessage;
+    return new Promise((resolve) => {
+        let zip = new JSZip();
+        for (let data of exportableBytes) {
+            console.log("data =>", data);
+            const { bytes, name, setting } = data;
+            const cleanBytes = typedArrayToBuffer(bytes);
+            const type = exportTypeToBlobType(setting.format);
+            const extension = exportTypeToFileExtension(setting.format);
+            let blob = new Blob([cleanBytes], { type });
+            zip.file(`${name}${setting.suffix}${extension}`, blob, {
+                base64: true,
+            });
+        }
+        zip.generateAsync({ type: "blob" }).then((content) => {
+            const blobURL = window.URL.createObjectURL(content);
+            const link = document.createElement("a");
+            link.className = "button button--primary";
+            link.href = blobURL;
+            link.download = "export.zip";
+            link.click();
+            link.setAttribute("download", name + ".zip");
+            resolve();
+        });
+    }).then(() => {
+        window.parent.postMessage({ pluginMessage: "Done!" }, "*");
+    });
+};
